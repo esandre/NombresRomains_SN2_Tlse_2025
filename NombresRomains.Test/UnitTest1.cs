a38fdb648ec5cab4051be2a9ad81c82239061dd2
Enzo Sandre <enzo@enzosandre.fr>
2025-03-26 10:27:36 +0100

    Version factorisée, niveau 2

--- NombresRomains.Test/UnitTest1.cs
+++ NombresRomains.Test/UnitTest1.cs
@@ -11,6 +11,36 @@ public class UnitTest1
         Assert.Equal(new string('I', nombreArabe), nombreArabe.ToRomanNumbers());
     }
 
+    [Theory]
+    [InlineData("V", 5)]
+    [InlineData("X", 10)]
+    [InlineData("XV", 15)]
+    [InlineData("XX", 20)]
+    public void SymbolesRemarquables(string representation, int valeur)
+    {
+        Assert.Equal(representation, valeur.ToRomanNumbers());
+    }
+
+    [Theory(DisplayName = "Entre 1 et 3 au-dessus d'un symbole, on répète n - valeurSymbole * I")]
+    [InlineData(5, 1)]
+    [InlineData(5, 2)]
+    [InlineData(5, 3)]
+    [InlineData(10, 1)]
+    [InlineData(10, 2)]
+    [InlineData(10, 3)]
+    [InlineData(15, 1)]
+    [InlineData(15, 2)]
+    [InlineData(15, 3)]
+    public void TestUnitésAuDessusSymbole(int valeurSymbole, int deltaAvecValeurSymbole)
+    {
+        var representationSymbole = valeurSymbole.ToRomanNumbers();
+        var attendu = representationSymbole + new string('I', deltaAvecValeurSymbole);
+
+        var nombreTesté = valeurSymbole + deltaAvecValeurSymbole;
+
+        Assert.Equal(attendu, nombreTesté.ToRomanNumbers());
+    }
+
     [Theory(DisplayName = "Une unité avant un symbole s'écrit avec I précédant ce symbole")]
     [InlineData(4)]
     [InlineData(9)]
@@ -24,43 +54,6 @@ public class UnitTest1
 
         var attendu = representationSymbole.Insert(representationSymbole.Length - 1, "I");
         Assert.Equal(attendu, nombreRomainTesté);
-    }
-
-    [Theory(DisplayName = "Entre 5 et 8, on met V plus (5-n) * I")]
-    [InlineData(5)]
-    [InlineData(6)]
-    [InlineData(7)]
-    [InlineData(8)]
-    public void TestCinqPlusUnité(int nombreArabe)
-    {
-        Assert.Equal("V" + new string('I', nombreArabe - 5), nombreArabe.ToRomanNumbers());
-    }
-
-    [Theory(DisplayName = "Entre 10 et 13, on met X plus (10-n) * I")]
-    [InlineData(10)]
-    [InlineData(11)]
-    [InlineData(12)]
-    [InlineData(13)]
-    public void TestDixPlusUnité(int nombreArabe)
-    {
-        Assert.Equal("X" + new string('I', nombreArabe - 10), nombreArabe.ToRomanNumbers());
-    }
-
-    [Theory(DisplayName = "Entre 15 et 18, on met XV plus (15-n) * I")]
-    [InlineData(15)]
-    [InlineData(16)]
-    [InlineData(17)]
-    [InlineData(18)]
-    public void TestQuinzePlusUnité(int nombreArabe)
-    {
-        Assert.Equal("XV" + new string('I', nombreArabe - 15), nombreArabe.ToRomanNumbers());
-    }
-
-    [Fact]
-    public void Test20()
-    {
-        const int nombre = 20;
-        Assert.Equal("XX", nombre.ToRomanNumbers());
     }
 
     [Fact]
